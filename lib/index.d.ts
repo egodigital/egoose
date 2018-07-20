53f468dd3b5fd7b96ac068c60002e53dea77f024
Marcel Kloubert <kloubert@egoag.local>
2018-07-20 08:20:55 +0200

    updated for npm

--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function toStringSafe(val: any, defaultValue?: string): string;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 /**
- * This file is part of the egoose distribution.
+ * This file is part of the @egodigital/egoose distribution.
  * Copyright (c) Marcel Joachim Kloubert.
  *
- * egoose is free software: you can redistribute it and/or modify
+ * @egodigital/egoose is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, version 3.
  *
- * egoose is distributed in the hope that it will be useful, but
+ * @egodigital/egoose is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
@@ -16,6 +16,91 @@
  */
 
 import * as _ from 'lodash';
+
+/**
+ * Returns an input value as array.
+ *
+ * @param {T|T[]} val The input value.
+ * @param {boolean} [noEmpty] Remove values, which are (null) or (undefined).
+ *
+ * @return {T[]} The input value as array.
+ */
+export function asArray<T>(val: T | T[], noEmpty = true): T[] {
+    if (!Array.isArray(val)) {
+        val = [val];
+    }
+
+    return val.filter(x => {
+        if (noEmpty) {
+            return !_.isNil(x);
+        }
+
+        return true;
+    });
+}
+
+/**
+ * Compare to values for sorting.
+ *
+ * @param {any} x The "left" value.
+ * @param {any} y The "right" value.
+ *
+ * @return {number} The sort value.
+ */
+export function compareValues<T = any>(x: T, y: T): number {
+    return compareValuesBy(x, y,
+                           i => i);
+}
+
+/**
+ * Compare to values for sorting by using a selector.
+ *
+ * @param {any} x The "left" value.
+ * @param {any} y The "right" value.
+ * @param {Function} selector The selector.
+ *
+ * @return {number} The sort value.
+ */
+export function compareValuesBy<T = any, U = T>(x: T, y: T, selector: (val: T) => U): number {
+    const VAL_X = selector(x);
+    const VAL_Y = selector(y);
+
+    if (VAL_X !== VAL_Y) {
+        if (VAL_X < VAL_Y) {
+            return -1;
+        }
+
+        if (VAL_X > VAL_Y) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * Checks if the string representation of a value is an empty string or not.
+ *
+ * @param {any} val The value to check.
+ *
+ * @return {boolean} If empty string or not.
+ */
+export function isEmptyString(val: any): boolean {
+    return '' === toStringSafe(val)
+        .trim();
+}
+
+/**
+ * Normalizes a value to a string, which is comparable.
+ *
+ * @param {any} val The value to normalize.
+ *
+ * @return {string} The normalized string.
+ */
+export function normalizeString(val: any): string {
+    return toStringSafe(val).toLowerCase()
+        .trim();
+}
 
 /**
  * Converts a value to a string (if needed), which is not (null) and not (undefined).
