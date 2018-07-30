c38b3b253e418bfec11a704fc89343bcbfd11aff
Marcel Kloubert <marcel.kloubert@gmx.net>
2018-07-30 23:49:42 +0200

    async tests for applyFuncFor

--- src/test/applyFuncFor.ts
+++ src/test/applyFuncFor.ts
@@ -21,7 +21,7 @@ import { applyFuncFor } from '../index';
 
 describe('#applyFuncFor()', function() {
     describe('(null)', function() {
-        it('should use OBJ_2 as thisArg instead of OBJ_1', function() {
+        it('should use OBJ_2 as thisArg instead of OBJ_1 (sync)', function() {
             let objOfFunc: any;
 
             const OBJ_1: any = {
@@ -44,6 +44,41 @@ describe('#applyFuncFor()', function() {
 
                     const EXPECTED_SUM = a + b;
                     const ACTUAL_SUM = FUNC(a, b);
+
+                    assert.ok('number' === typeof ACTUAL_SUM);
+                    assert.equal(ACTUAL_SUM, EXPECTED_SUM);
+                    assert.strictEqual(ACTUAL_SUM, EXPECTED_SUM);
+
+                    assert.ok('object' === typeof objOfFunc);
+                    assert.equal(objOfFunc, OBJ_2);
+                    assert.strictEqual(objOfFunc, OBJ_2);
+                }
+            }
+        });
+
+        it('should use OBJ_2 as thisArg instead of OBJ_1 (async)', async function() {
+            let objOfFunc: any;
+
+            const OBJ_1: any = {
+                test: async function(a: number, b: number) {
+                    objOfFunc = this;
+
+                    return a + b;
+                },
+            };
+            const OBJ_2: any = {};
+
+            const FUNC = applyFuncFor(OBJ_1.test, OBJ_2);
+            assert.ok('function' === typeof FUNC);
+            assert.notEqual(FUNC, OBJ_1.test);
+            assert.notStrictEqual(FUNC, OBJ_1.test);
+
+            for (let a = 0; a < 100; a++) {
+                for (let b = 0; b < 100; b++) {
+                    objOfFunc = false;
+
+                    const EXPECTED_SUM = a + b;
+                    const ACTUAL_SUM = await FUNC(a, b);
 
                     assert.ok('number' === typeof ACTUAL_SUM);
                     assert.equal(ACTUAL_SUM, EXPECTED_SUM);
